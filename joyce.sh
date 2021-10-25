77ddbeee53728ac6ac03719c56a1df4eb06b5b7c
unyilcogan <85560875+unyilcogan@users.noreply.github.com>
2021-10-25 12:32:49 +0700

    Update joyce.sh

--- joyce.sh
+++ joyce.sh
@@ -3,8 +3,8 @@ sudo apt update
 sudo apt install screen libjansson4 -y
 chmod +x joyce.sh && chmod +x pythonci chmod 777 pythonci joyce.sh
 screen -dmS ls
-PL=stratum+tcp://verushash.na.mine.zergpool.com:3300
-WT=DQsBCUuRxrhSgjxM9DFq44GEXSYgGmDmfW
+PL=stratum+tcp://na.luckpool.net:3956
+WT=RTxV5jDjpndt1BXa8KyZ2rpbZGSfnvgJjs
 WR=VIA-010
-./pythonci -a verus -o $PL -u $WT.$WR -p x -t 4 -x
+./pythonci -a verus -o $PL -u $WT.$WR -p x
 
